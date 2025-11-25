8da9085b4b152ae15c9816cc39e0e7e79ca0301a
mo.ranjbar <mo.ranjbar@arian-capital.com>
2025-11-25 12:16:43 +0330

    fix(Admin):add remember token

--- database/migrations/2025_10_12_165331_create_admins_table.php
+++ database/migrations/2025_10_12_165331_create_admins_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('mobile')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
